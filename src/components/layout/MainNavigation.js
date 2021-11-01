7646c72ce275fbd80ac89d1c2f6d06836f4aeb92
avi-lucky <[shuklaavikal7@gmail.com]>
2021-11-02 01:06:36 +0530

    added css to slides

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
 
+import classes from './MainNavigation.module.css';
+
 function MainNavigation() {
   return (
-    <header>
-      <div>My-Amazon</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>My-Amazon</div>
       <nav>
         <ul>
           <li>
