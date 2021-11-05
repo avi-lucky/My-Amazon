c868124ce63e19108b2def1abb1733ed7adc7e85
avi-lucky <[shuklaavikal7@gmail.com]>
2021-11-05 23:33:02 +0530

    app successfully connected with firebase database

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,43 +1,80 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
-import ShopList from '../components/shopping/ShopList';
+import ShopList from "../components/shopping/ShopList";
 
 const DUMMY_DATA = [
-  {
-    id: "m1",
-    title: "HandCuffs",
-    image: "https://m.media-amazon.com/images/I/51sc5vnUdDL.jpg",
-    description:
-      "This is a first, amazing cap which you definitely should not ignore. It will be a lot of fun!",
-  },
-  {
-    id: "m2",
-    title: "Clothera",
-    image: "https://m.media-amazon.com/images/I/7151VjrBsNS._UX679_.jpg",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  }, 
-  {
-    id: "m3",
-    title: "Hoodies",
-    image: "https://m.media-amazon.com/images/I/51hFAXgwMUS._UL1500_.jpg",
-    description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-  },
-];
+    {
+      id: "m1",
+      title: "HandCuffs",
+      image: "https://m.media-amazon.com/images/I/51sc5vnUdDL.jpg",
+      description:
+        "This is a first, amazing cap which you definitely should not ignore. It will be a lot of fun!",
+    },
+    {
+      id: "m2",
+      title: "Clothera",
+      image: "https://m.media-amazon.com/images/I/7151VjrBsNS._UX679_.jpg",
+      description:
+        "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+    }, 
+    {
+      id: "m3",
+      title: "Hoodies",
+      image: "https://m.media-amazon.com/images/I/51hFAXgwMUS._UL1500_.jpg",
+      description:
+        "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+    },
+  ];
+  
 
 function Shop() {
-    fetch('https://react-shopping-app-4c30c-default-rtdb.firebaseio.com/shopping.json'
-    ).then(response => {
-       return response.json();
-    }).then(data => {
-        return data
-    });
+  const [isLoading, setIsLoading] = useState(true);
+//   const [setLoadedShopping] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-shopping-app-4c30c-default-rtdb.firebaseio.com/shopping.json",
+      {
+          method: 'POST',
+          body: JSON.stringify(DUMMY_DATA),
+          headers: {
+              'Content-Type': 'application/json'
+          }
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const shopping = [];
+
+        for (const key in data) {
+          const shop = {
+            id: key,
+            ...data[key],
+          };
+
+          shopping.push(shop);
+        }
+
+        setIsLoading(false);
+        // setLoadedShopping(shopping);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
 
   return (
     <section>
       <h1>Shop</h1>
-       <ShopList shopping={DUMMY_DATA} />
+      <ShopList shopping={DUMMY_DATA} />
     </section>
   );
 }
