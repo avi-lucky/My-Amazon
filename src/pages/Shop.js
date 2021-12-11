84495e6d04abf3765866e2b7524033f4f8f07013
avi-lucky <[shuklaavikal7@gmail.com]>
2021-12-11 16:39:28 +0530

    Retrive data from firebase

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -2,49 +2,41 @@ import { useState, useEffect } from "react";
 
 import ShopList from "../components/shopping/ShopList";
 
-const DUMMY_DATA = [
-    {
-      id: "m1",
-      title: "HandCuffs",
-      image: "https://m.media-amazon.com/images/I/51sc5vnUdDL.jpg",
-      price: 150,
-      description:
-        "This is a first, amazing cap which you definitely should not ignore. It will be a lot of fun!",
-    },
-    {
-      id: "m2",
-      title: "Clothera",
-      image: "https://m.media-amazon.com/images/I/7151VjrBsNS._UX679_.jpg",
-      price: 350,
-      description:
-        "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-    }, 
-    {
-      id: "m3",
-      title: "Hoodies",
-      image: "https://m.media-amazon.com/images/I/51hFAXgwMUS._UL1500_.jpg",
-      price: 560,
-      description:
-        "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-    },
-  ];
-  
+// const DUMMY_DATA = [
+//     {
+//       id: "m1",
+//       title: "HandCuffs",
+//       image: "https://m.media-amazon.com/images/I/51sc5vnUdDL.jpg",
+//       price: 150,
+//       description:
+//         "This is a first, amazing cap which you definitely should not ignore. It will be a lot of fun!",
+//     },
+//     {
+//       id: "m2",
+//       title: "Clothera",
+//       image: "https://m.media-amazon.com/images/I/7151VjrBsNS._UX679_.jpg",
+//       price: 350,
+//       description:
+//         "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+//     },
+//     {
+//       id: "m3",
+//       title: "Hoodies",
+//       image: "https://m.media-amazon.com/images/I/51hFAXgwMUS._UL1500_.jpg",
+//       price: 560,
+//       description:
+//         "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+//     },
+//   ];
 
 function Shop() {
   const [isLoading, setIsLoading] = useState(true);
-//   const [setLoadedShopping] = useState([]);
+  const [loadedShopping, setLoadedShopping] = useState([]);
 
   useEffect(() => {
     setIsLoading(true);
     fetch(
-      "https://react-shopping-app-4c30c-default-rtdb.firebaseio.com/shopping.json",
-      {
-          method: 'POST',
-          body: JSON.stringify(DUMMY_DATA),
-          headers: {
-              'Content-Type': 'application/json'
-          }
-      }
+      "https://shopping-app-64fa6-default-rtdb.firebaseio.com/shopping.json"
     )
       .then((response) => {
         return response.json();
@@ -52,17 +44,19 @@ function Shop() {
       .then((data) => {
         const shopping = [];
 
-        for (const key in data) {
-          const shop = {
-            id: key,
-            ...data[key],
-          };
-
-          shopping.push(shop);
+        for (var propName in data) {
+          if (data.hasOwnProperty(propName)) {
+            var propValue = data[propName];
+          }
         }
+        console.log(propValue);
+        propValue.forEach(element => {
+          shopping.push(element)
+        });
+        console.log(shopping)
 
         setIsLoading(false);
-        // setLoadedShopping(shopping);
+        setLoadedShopping(shopping);
       });
   }, []);
 
@@ -77,7 +71,7 @@ function Shop() {
   return (
     <section>
       <h1>Shop</h1>
-      <ShopList shopping={DUMMY_DATA} />
+      <ShopList shopping={loadedShopping} />
     </section>
   );
 }
